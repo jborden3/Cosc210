6ca1d0d99b9edb6be7083216c94baed93b78a6fa
borde <borde@172.31.157.204>
2019-10-11 10:56:55 -0500

    3rd homework

--- src/main/java/edu/samford/jonathanborde/iterators/NoisyFilter.java
+++ src/main/java/edu/samford/jonathanborde/iterators/NoisyFilter.java
@@ -17,13 +17,19 @@ public class NoisyFilter {
     static DLinkedList<Integer> temps = new DLinkedList<>();
 
     public static void main(String[] args) {
-        generateNoisyData(90, 110, 0.01);
-        double avg1 = calculateAverage();
+        double rate = 0.04;
+        double avg1;
+        double avg2;
+        do {
+        generateNoisyData(90, 110, rate);
+         avg1 = calculateAverage();
         System.out.println("Average with noisy data" + avg1);
         filterNoisyData(90, 110);
-        double avg2 = calculateAverage();
+         avg2 = calculateAverage();
         System.out.println("Average without noisy data" + avg2);
-
+        rate = rate + .0001;
+        } while (Math.abs(avg2-avg1)<1);
+        System.out.println("Final rate: " + rate);
     }
 
     /**
@@ -35,6 +41,7 @@ public class NoisyFilter {
      */
     private static void generateNoisyData(int low, int high, double noiseprob) {
         java.util.Random r = new java.util.Random();
+        temps = new DLinkedList<>();
         for (int i = 0; i < 1000; i++) {
             if (Math.random() < noiseprob) {
                 // genrate a nosiy reading
